cd6dc94771b4550dcc2c9197102615c3934c5b6e
Denali Tonn <87244677+DenaliTonn@users.noreply.github.com>
2022-11-30 20:55:41 -0800

    finished

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBmFBpz4YhIf7ohSKxD2EKHMI87zZ7M-M",
-  authDomain: "blog-code-along-11-6.firebaseapp.com",
-  projectId: "blog-code-along-11-6",
-  storageBucket: "blog-code-along-11-6.appspot.com",
-  messagingSenderId: "374105721566",
-  appId: "1:374105721566:web:8bfdd20823adcbe41a104f",
+  apiKey: "AIzaSyDfVH0MtxruGKPYMc3Y8CJWX6XrLzm6rhs",
+  authDomain: "twooter-88835.firebaseapp.com",
+  projectId: "twooter-88835",
+  storageBucket: "twooter-88835.appspot.com",
+  messagingSenderId: "448456032623",
+  appId: "1:448456032623:web:80ee8a9479cd6c5226e89a",
 };
 
 export const app = initializeApp(firebaseConfig);
